be738d3fc3ab9148af36f7386a04600d5776e4b5
Aisuko <urakiny@gmail.com>
2024-11-28 22:54:51 +0000

    Add script for download data
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- scripts/obgn_arxiv.py
+++ scripts/obgn_arxiv.py
@@ -1,13 +1,13 @@
-# from ogb.nodeproppred import PygNodePropPredDataset
+from ogb.nodeproppred import PygNodePropPredDataset
 # from ogb.graphproppred import PygGraphPropPredDataset
 
 # Specify the desired directory
-root_dir = '.'
+root_dir = './data'
 
 if __name__ =="__main__":
 
     # Load the dataset with the specified root directory
-    # dataset = PygNodePropPredDataset(name='ogbn-arxiv', root=root_dir)
+    dataset = PygNodePropPredDataset(name='ogbn-arxiv', root=root_dir)
     # dataset = PygGraphPropPredDataset(name = 'ogbg-molhiv')
     # print(len(dataset))
     print("hello")
